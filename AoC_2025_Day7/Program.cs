8fc7b7cd7450c03083de1d169f9e9d221bdda7fb
Mike Dunn <mdunn147@gmail.com>
2025-12-07 09:13:18 +0000

    Added day 7 project

---
+++ AoC_2025_Day7/Program.cs
@@ -0,0 +1,30 @@
+﻿using AoC.Utilities;
+
+namespace AoC_2025_Day7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("filename is required!");
+            throw new Exception("No file provided!");
+        }
+
+        SolvePart(args[0], 1);
+
+        //SolvePart(args[0], 2);
+    }
+
+    private static void SolvePart(string inputFile, int partNumber)
+    {
+        Console.WriteLine($"Part {partNumber}:");
+        if (partNumber != 1 && partNumber != 2)
+        {
+            throw new Exception("Invalid part number!");
+        }
+
+        
+    }
+}
