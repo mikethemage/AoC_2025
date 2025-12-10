60d7e7d8961ac4b896b444373386cb2b54409ac8
Mike Dunn <mdunn147@gmail.com>
2025-12-10 10:00:45 +0000

    Solved day 10 part 1

---
+++ AoC_2025_Day10/Program.cs
@@ -0,0 +1,159 @@
+﻿using AoC.Utilities;
+
+namespace AoC_2025_Day10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("filename is required!");
+            throw new Exception("No file provided!");
+        }
+
+        SolvePart(args[0], 1);
+
+        //SolvePart(args[0], 2);
+    }
+
+    private static void SolvePart(string inputFile, int partNumber)
+    {
+        Console.WriteLine($"Part {partNumber}:");
+        if (partNumber != 1 && partNumber != 2)
+        {
+            throw new Exception("Invalid part number!");
+        }
+
+        List<Machine> machines = LoadMachines(inputFile);
+
+        int machineId = 1;
+        int totalButtonPresses = 0;
+        foreach (Machine machine in machines)
+        {
+            int buttonPressesRequired = GetMinimumButtonPresses(machine, partNumber!=1);
+            Console.WriteLine($"Machine {machineId}: Minimum button presses: {buttonPressesRequired}");
+            totalButtonPresses += buttonPressesRequired;
+            machineId++;
+        }
+        Console.WriteLine($"Total button presses: {totalButtonPresses}");
+    }
+
+    private static int GetMinimumButtonPresses(Machine machine, bool useJoltages)
+    {
+        List<bool> initialLightStatus = new List<bool>();
+        foreach (bool light in machine.TargetLightStatus)
+        {
+            initialLightStatus.Add(false);
+        }
+        LightState initialLightState = new LightState(initialLightStatus);
+        Queue<LightState> lightStates = new Queue<LightState>();
+        lightStates.Enqueue(initialLightState);
+
+        while (lightStates.Count > 0)
+        {
+            LightState currentState = lightStates.Dequeue();
+            if (TargetStatusReached(currentState.CurrentLightStatus, machine.TargetLightStatus))
+            {
+                return currentState.ButttonsPressed.Count;
+            }
+            for (int i = 0; i < machine.Buttons.Count; i++)
+            {
+                if (currentState.ButttonsPressed.Count < 1 || currentState.ButttonsPressed.Last() != i)
+                {
+                    int priority = 1;
+                    if (useJoltages)
+                    {
+                        priority = machine.JoltageRequirements[i];
+                    }
+
+                    lightStates.Enqueue(machine.PressButton(currentState, i));
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static bool TargetStatusReached(List<bool> currentLightStatus, List<bool> targetLightStatus)
+    {
+        for (int i = 0; i < targetLightStatus.Count; i++)
+        {
+            if (currentLightStatus[i] != targetLightStatus[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Machine> LoadMachines(string inputFile)
+    {
+        List<Machine> machines = new List<Machine>();
+        List<string> lines = InputParser.ReadInputAsRows(inputFile);
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                machines.Add(LoadMachine(line));
+            }
+        }
+
+        return machines;
+    }
+
+    private static Machine LoadMachine(string line)
+    {
+        string[] parts1 = line.Split("] ", StringSplitOptions.TrimEntries);
+        string[] parts2 = parts1[1].Split(" {", StringSplitOptions.TrimEntries);
+
+        string targetLightString = parts1[0].Trim().TrimStart('[');
+        string buttonsString = parts2[0].Trim();
+        string joltageString = parts2[1].Trim().TrimEnd('}');
+
+        List<bool> targetLightStates = targetLightString.Select(x => x == '#').ToList();
+        List<List<int>> buttons = buttonsString.Split(' ', StringSplitOptions.TrimEntries)
+                                                .Select(x => x.TrimStart('(')
+                                                                .TrimEnd(')')
+                                                                .Split(',', StringSplitOptions.TrimEntries)
+                                                                .Select(y => int.Parse(y))
+                                                                .ToList())
+                                                .ToList();
+        List<int> joltages = joltageString.Split(',', StringSplitOptions.TrimEntries).Select(x => int.Parse(x)).ToList();
+
+        Machine output = new Machine { Buttons = buttons, JoltageRequirements = joltages, TargetLightStatus = targetLightStates };
+        return output;
+    }
+}
+
+internal class Machine
+{
+    public required List<bool> TargetLightStatus { get; init; }
+    public required List<List<int>> Buttons { get; init; }
+    public required List<int> JoltageRequirements { get; init; }
+    public LightState PressButton(LightState currentLightState, int button)
+    {
+        return currentLightState.Pressbutton(button, Buttons[button]);
+    }
+}
+
+internal class LightState
+{
+    public List<int> ButttonsPressed { get; private set; } = new List<int>();
+    public List<bool> CurrentLightStatus { get; private set; }
+    public LightState(List<bool> initialLightStatus)
+    {
+        CurrentLightStatus = new List<bool>(initialLightStatus);
+    }
+    public LightState Pressbutton(int buttonId, List<int> toggleLights)
+    {
+        List<bool> nextLightStatus = new List<bool>(CurrentLightStatus);
+        foreach (int toggleLight in toggleLights)
+        {
+            nextLightStatus[toggleLight] = !nextLightStatus[toggleLight];
+        }
+        LightState nextLightState = new LightState(nextLightStatus);
+        nextLightState.ButttonsPressed.AddRange(ButttonsPressed);
+        nextLightState.ButttonsPressed.Add(buttonId);
+        return nextLightState;
+    }
+}
--- AoC_2025_Day9/Program.cs
+++ AoC_2025_Day9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AoC.Utilities;
+using System.Drawing;
 
 namespace AoC_2025_Day9;
 
@@ -14,7 +15,7 @@ internal class Program
 
         SolvePart(args[0], 1);
 
-        //SolvePart(args[0], 2);
+        SolvePart(args[0], 2);
     }
 
     private static void SolvePart(string inputFile, int partNumber)
@@ -39,16 +40,118 @@ internal class Program
             }
         }
 
-        Rectangle? biggestRectangle = rectangles.MaxBy(x => x.Area);
-        //VisualizeTiles(tiles, biggestRectangle);
-        //Console.WriteLine();
 
-        if (biggestRectangle is not null)
+        if (partNumber == 1)
         {
-            Console.WriteLine($"Biggest Rectangle Area: {biggestRectangle.Area}");
+            Rectangle? biggestRectangle = rectangles.MaxBy(x => x.Area);
+            //VisualizeTiles(tiles, biggestRectangle);
+            //Console.WriteLine();
+
+            if (biggestRectangle is not null)
+            {
+                Console.WriteLine($"Biggest Rectangle Area: {biggestRectangle.Area}");
+            }
+        }
+        else
+        {
+            //Order rectangles by area:
+            rectangles = rectangles.OrderByDescending(x => x.Area).ToList();
+
+            //Todo - calculate bounding borders and use in validation.
+            List<Line> borderLine = GetBorderLines(tiles);
+           
+
+            //VisualizeTilesWithFilled(tiles, completedFilled);
+            //Console.WriteLine();
+
+            foreach (Rectangle rectangle in rectangles)
+            {
+                if (IsValidRectangle(rectangle, entireBorder))
+                {
+                    //VisualizeTiles(tiles, rectangle);
+                    //Console.WriteLine();
+                    Console.WriteLine($"Biggest Valiid Rectangle Area: {rectangle.Area}");
+                    break;
+                }
+            }
+        }
+    }
+
+    private static List<Line> GetBorderLines(List<Tile> tiles)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static bool IsValidRectangle(Rectangle rectangle, List<Tile> entireBorder)
+    {
+        if(    !ValidBoundingCorner(rectangle.Tile1.Row,rectangle.Tile1.Column, entireBorder)
+            || !ValidBoundingCorner(rectangle.Tile1.Row, rectangle.Tile2.Column, entireBorder)
+            || !ValidBoundingCorner(rectangle.Tile2.Row, rectangle.Tile2.Column, entireBorder)
+            || !ValidBoundingCorner(rectangle.Tile2.Row, rectangle.Tile1.Column, entireBorder)
+            )
+        {
+            return false;
         }
 
+        //Check each side of the rectangle for crossing borders
+
     }
+
+    private static bool ValidBoundingCorner(int row, int column, List<Tile> entireBorder)
+    {
+        if(entireBorder.Where(x=>x.Row == row && x.Column < column).Count() % 2 == 0
+            && entireBorder.Where(x => x.Row == row && x.Column <= column).Count() % 2 == 0
+            )
+        {
+            return false;
+        }
+
+        if (entireBorder.Where(x => x.Column == column && x.Row < row).Count() % 2 == 0
+            && entireBorder.Where(x => x.Column == column && x.Row <= row).Count() % 2 == 0
+            )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static List<Tile> GetBorderTiles(List<Tile> tiles)
+    {
+        List<Tile> borderTiles = new List<Tile>();
+        Tile previousTile = tiles[0];
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            Tile currentTile;
+            if(i==tiles.Count-1)
+            {
+                currentTile = tiles[0];
+            }
+            else
+            {
+                currentTile = tiles[i+1];
+            }
+            
+            if (currentTile.Row == previousTile.Row)
+            {
+                for (int j = Math.Min(currentTile.Column, previousTile.Column) + 1; j < Math.Max(currentTile.Column, previousTile.Column); j++)
+                {
+                    borderTiles.Add(new Tile { Column = j, Row = currentTile.Row });
+                }
+            }
+            if (currentTile.Column == previousTile.Column)
+            {
+                for (int j = Math.Min(currentTile.Row, previousTile.Row) + 1; j < Math.Max(currentTile.Row, previousTile.Row); j++)
+                {
+                    borderTiles.Add(new Tile { Row = j, Column = currentTile.Column });
+                }
+            }
+            previousTile = currentTile;
+        }
+        return borderTiles;
+    }
+
+    
 
     private static void VisualizeTiles(List<Tile> tiles, Rectangle? rectangle = null)
     {
@@ -78,6 +181,8 @@ internal class Program
         }
     }
 
+    
+
     private static List<Tile> LoadTiles(string inputFile)
     {
         List<Tile> tiles = new List<Tile>();
@@ -89,4 +194,9 @@ internal class Program
 
         return tiles;
     }
+}
+
+internal class Line
+{
+
 }
