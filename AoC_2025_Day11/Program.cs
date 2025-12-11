84161f04c0744156409518e78e69fa95c3bc0c39
Mike Dunn <mdunn147@gmail.com>
2025-12-11 09:45:43 +0000

    Solved Day 11 Part 1

--- AoC_2025_Day11/Program.cs
+++ AoC_2025_Day11/Program.cs
@@ -25,7 +25,44 @@ internal class Program
             throw new Exception("Invalid part number!");
         }
 
+        Dictionary<string, List<string>> devices = LoadDevices(inputFile);
 
-        
+        Queue<string> paths = new Queue<string>();
+        paths.Enqueue("you");
+        int pathCount = 0;
+        while(paths.Count>0)
+        {
+            string currentDevice = paths.Dequeue();
+            if (currentDevice == "out")
+            {
+                pathCount++;
+            }
+            else
+            {
+                foreach (string nextPath in devices[currentDevice])
+                {
+                    paths.Enqueue(nextPath);
+                }
+            }
+        }
+        Console.WriteLine(pathCount);
+
+    }
+
+    private static Dictionary<string, List<string>> LoadDevices(string inputFile)
+    {
+        var lines = InputParser.ReadInputAsRows(inputFile);
+        Dictionary<string, List<string>> devices = new Dictionary<string, List<string>>();
+        foreach (string line in lines)
+        {
+            string[] parts1 = line.Split(": ", StringSplitOptions.TrimEntries);
+            string[] parts2 = parts1[1].Split(" ", StringSplitOptions.TrimEntries);
+
+            string deviceName = parts1[0];
+            List<string> outputs = parts2.ToList();
+            devices.Add(deviceName, outputs);
+        }
+
+        return devices;
     }
 }
