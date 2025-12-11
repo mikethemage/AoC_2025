419e7d230b7e2f914fca68598017bebd4d15f8f3
Mike Dunn <mdunn147@gmail.com>
2025-12-11 13:25:19 +0000

    First attempt at Day 11 Part 2 - gives correct example but wrong for input

--- AoC_2025_Day11/Program.cs
+++ AoC_2025_Day11/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length < 1)
+        if (args.Length < 2)
         {
             Console.WriteLine("filename is required!");
             throw new Exception("No file provided!");
@@ -14,7 +14,7 @@ internal class Program
 
         SolvePart(args[0], 1);
 
-        //SolvePart(args[0], 2);
+        SolvePart(args[1], 2);
     }
 
     private static void SolvePart(string inputFile, int partNumber)
@@ -27,26 +27,66 @@ internal class Program
 
         Dictionary<string, List<string>> devices = LoadDevices(inputFile);
 
-        Queue<string> paths = new Queue<string>();
-        paths.Enqueue("you");
-        int pathCount = 0;
-        while(paths.Count>0)
+        if(partNumber==1)
         {
-            string currentDevice = paths.Dequeue();
-            if (currentDevice == "out")
+            Queue<string> paths = new Queue<string>();
+            paths.Enqueue("you");
+            int pathCount = 0;
+            while (paths.Count > 0)
             {
-                pathCount++;
-            }
-            else
-            {
-                foreach (string nextPath in devices[currentDevice])
+                string currentDevice = paths.Dequeue();
+                if (currentDevice == "out")
                 {
-                    paths.Enqueue(nextPath);
+                    pathCount++;
+                }
+                else
+                {
+                    foreach (string nextPath in devices[currentDevice])
+                    {
+                        paths.Enqueue(nextPath);
+                    }
                 }
             }
+            Console.WriteLine(pathCount);
         }
-        Console.WriteLine(pathCount);
+        else
+        {
+            Dictionary<(string currentNode, bool visitedDac, bool visitedFft), int> memory = new Dictionary<(string currentNode, bool visitedDac, bool visitedFft), int>();
+            memory.Add(("svr", false, false), 1);
+            Stack<PathInfo> paths = new Stack<PathInfo>();
+            paths.Push(new PathInfo { CurrentNode = "svr", VisitedDac = false, VisitedFft = false });
+            while (paths.Count>0)
+            {
+                PathInfo currentDevice = paths.Pop();
 
+                if (devices.ContainsKey(currentDevice.CurrentNode))
+                {
+                    List<string> nextDevices = devices[currentDevice.CurrentNode];
+                    foreach (var nextDevice in nextDevices.Select(x=>new PathInfo { CurrentNode=x, VisitedDac=currentDevice.VisitedDac, VisitedFft=currentDevice.VisitedFft}).ToList())
+                    {
+                        if (nextDevice.CurrentNode == "dac")
+                        {
+                            nextDevice.VisitedDac = true;
+                        }
+                        if (nextDevice.CurrentNode == "fft")
+                        {
+                            nextDevice.VisitedFft = true;
+                        }
+                        if (memory.ContainsKey((nextDevice.CurrentNode, nextDevice.VisitedDac, nextDevice.VisitedFft)))
+                        {
+                            memory[(nextDevice.CurrentNode, nextDevice.VisitedDac, nextDevice.VisitedFft)] += memory[(currentDevice.CurrentNode, currentDevice.VisitedDac, currentDevice.VisitedFft)];
+                        }
+                        else
+                        {
+                            memory.Add((nextDevice.CurrentNode, nextDevice.VisitedDac, nextDevice.VisitedFft), memory[(currentDevice.CurrentNode, currentDevice.VisitedDac, currentDevice.VisitedFft)]);
+                            paths.Push(nextDevice);
+                        }
+                    }
+                }                
+            }
+
+            Console.WriteLine(memory[("out", true, true)]);           
+        }
     }
 
     private static Dictionary<string, List<string>> LoadDevices(string inputFile)
@@ -66,3 +106,12 @@ internal class Program
         return devices;
     }
 }
+
+internal class PathInfo
+{
+    public required string CurrentNode { get; set; }
+    public bool VisitedDac { get; set; } = false;
+    public bool VisitedFft { get; set; } = false;
+
+}
+
